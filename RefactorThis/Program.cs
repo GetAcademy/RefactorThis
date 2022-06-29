3686dc16a467d6049aa2d89447ea5cd36d0909c2
GET Academy <terje@kolderup.net>
2022-06-29 15:41:33 +0200

    x

--- RefactorThis/Program.cs
+++ RefactorThis/Program.cs
@@ -1,43 +1,7 @@
-﻿var vowels = "AEIOUYÆØÅ";
-var consonants = "BCDFGHJKLMNPQRSTVWXZ";
-var pattern = args[0].ToUpper();
-var characters = pattern.Select(c => c == 'V' ? 'A' : 'B').ToArray();
-while (true)
-{
-    Console.WriteLine(characters);
-    var index = characters.Length - 1;
-    var continueToNextCharacter = true;
-    while (continueToNextCharacter)
-    {
-        if (pattern[index] == 'V')
-        {
-            if (characters[index] == 'Å')
-            {
-                if (index == 0) return;
-                characters[index] = 'A';
-            }
-            else
-            {
-                var i = vowels.IndexOf(characters[index]);
-                characters[index] = vowels[i + 1];
-                continueToNextCharacter = false;
-            }
-        }
-        else if (pattern[index] == 'K')
-        {
-            if (characters[index] == 'Z')
-            {
-                if (index == 0) return;
-                characters[index] = 'B';
-            }
-            else
-            {
-                var i = consonants.IndexOf(characters[index]);
-                characters[index] = consonants[i + 1];
-                continueToNextCharacter = false;
-            }
-        }
+﻿using RefactorThis;
 
-        index--;
-    }
+var words = WordGenerator.Run("KVK");
+foreach (var word in words)
+{
+    Console.WriteLine(word);
 }
---
+++ RefactorThis/WordGenerator.cs
@@ -0,0 +1,38 @@
+﻿namespace RefactorThis
+{
+    internal class WordGenerator
+    {
+        public static List<string> Run(string pattern)
+        {
+            var characters = pattern
+                .ToUpper()
+                .Replace('V', 'A')
+                .Replace('K', 'B')
+                .ToCharArray();
+            var words = new List<string>();
+            while (true)
+            {
+                words.Add(new string(characters));
+                var index = 0;
+                for (index = characters.Length - 1; index >= 0; index--)
+                {
+                    var isVowel = pattern[index] == 'V';
+                    var listOfCharacters = isVowel ? "AEIOUYÆØÅ" : "BCDFGHJKLMNPQRSTVWXZ";
+                    var hasRestarted = GoToNextCharacter(index, listOfCharacters, characters);
+                    if (!hasRestarted) break;
+                }
+                var isLastWord = index == -1;
+                if (isLastWord) return words;
+            }
+        }
+
+        private static bool GoToNextCharacter(int index, string listOfCharacters, char[] characters)
+        {
+            var i = listOfCharacters.IndexOf(characters[index]);
+            var isLastCharacter = i == listOfCharacters.Length - 1;
+            var newIndex = isLastCharacter ? 0 : i + 1;
+            characters[index] = listOfCharacters[newIndex];
+            return isLastCharacter;
+        }
+    }
+}
